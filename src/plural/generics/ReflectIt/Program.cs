ae0de5bde1548bddd2d08237d63b0fea8ad84e57
Lakshan Fernando <lakshanf@hotmail.com>
2020-11-16 05:22:31 -0800

    Update Program.cs
    Adding debug info for the 3rd test case. IoCtests->Can_Resolve_Concrete_Type()

--- src/plural/generics/ReflectIt/Program.cs
+++ src/plural/generics/ReflectIt/Program.cs
@@ -13,6 +13,20 @@ namespace ReflectIt
             var o = Activator.CreateInstance(closeGenType);
             Console.WriteLine(o.GetType().Name);
             Console.WriteLine(o.GetType().FullName);
+            
+            // Debugging why InvoiceService cannot be Resolved - IoCtests->Can_Resolve_Concrete_Type()
+            // InvoiceService ctor requires implementations of IRepository<Customer> repository, ILogger logger
+            // The first parm has a path via the container map, IRepository<>=>SQLRepository<>
+            // SQLRepository<Customer> isn't in the map but can be done
+            // BUT SQLRepository doesnt have a default ctor - it has a ctor that takes a ILogger which needs to be managed
+            var openGenType2 = typeof(SQLRepositry<>);
+            var closeGenType2 = openGenType2.MakeGenericType(typeof(Customer));
+            var openGenType2ctor = closeGenType2.GetConstructors();
+            // Creating a concrete implementation of ILogger ctor for SQLRepository
+            var closed1 = Activator.CreateInstance(typeof(SQLServiceLogger));
+            var o2 = Activator.CreateInstance(closeGenType2, closed1);
+            Console.WriteLine(o2.GetType().Name);
+            Console.WriteLine(o2.GetType().FullName);            
 
             var t2 = typeof(Employee);
             var o2 = Activator.CreateInstance(t2);
