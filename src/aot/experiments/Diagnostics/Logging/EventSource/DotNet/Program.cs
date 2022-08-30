4a80ce1a233de1f0e4618766a0d0ce8d6443bc58
Lakshan Fernando <lakshanf@hotmail.com>
2022-08-30 12:27:22 -0700

    scripts

---
+++ src/aot/experiments/Diagnostics/Logging/EventSource/DotNet/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics.Tracing;
+
+namespace EventSourceDemo
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            DemoEventSource.Log.AppStarted("Hello World From .NET!", 12);
+            DemoEventSource.Log.DebugMessage("Got here From .NET!");
+            DemoEventSource.Log.DebugMessage("finishing startup From .NET!");
+            DemoEventSource.Log.RequestStart(3);
+            DemoEventSource.Log.RequestStop(3);
+        }
+    }
+
+    [EventSource(Name = "Demo")]
+    class DemoEventSource : EventSource
+    {
+        public static DemoEventSource Log { get; } = new DemoEventSource();
+
+        [Event(1, Keywords = Keywords.Startup)]
+        public void AppStarted(string message, int favoriteNumber) => WriteEvent(1, message, favoriteNumber);
+        [Event(2, Keywords = Keywords.Requests)]
+        public void RequestStart(int requestId) => WriteEvent(2, requestId);
+        [Event(3, Keywords = Keywords.Requests)]
+        public void RequestStop(int requestId) => WriteEvent(3, requestId);
+        [Event(4, Keywords = Keywords.Startup, Level = EventLevel.Verbose)]
+        public void DebugMessage(string message) => WriteEvent(4, message);
+
+
+        public class Keywords
+        {
+            public const EventKeywords Startup = (EventKeywords)0x0001;
+            public const EventKeywords Requests = (EventKeywords)0x0002;
+        }
+    }
+}
