56d40b0332ab2652bf881f5b92eb8a50298884c2
Lakshan Fernando <lakshanf@hotmail.com>
2022-09-30 07:39:36 -0700

    EP checkpoint

---
+++ src/aot/experiments/Diagnostics/Logging/PortEventPipe/RunEP.bat
@@ -0,0 +1,9 @@
+cls
+call taskkill /IM dotnet.exe /F
+git clean -xfd
+call build.cmd -arch x64 -os windows -s clr.aot -c Release > C:\Work\Core\LakshanF\CSharp\src\aot\experiments\Diagnostics\Logging\PortEventPipe\build\buildLog_change_41.log
+
+REM call build.cmd -arch x64 -os windows -s clr.aot -c Release -v:diag > C:\Work\Core\LakshanF\CSharp\src\aot\experiments\Diagnostics\Logging\PortEventPipe\build\buildLog_change_38.log
+
+
+REM call build.cmd -arch x64 -os windows -s clr -c Release -v:diag > C:\Work\Core\LakshanF\CSharp\src\aot\experiments\Diagnostics\Logging\PortEventPipe\build\buildLog_change_50.log
