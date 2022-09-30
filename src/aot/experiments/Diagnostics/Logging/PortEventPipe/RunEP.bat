45e4ac8017487272acb683a3c9086d07b01a25d0
Lakshan Fernando <lakshanf@hotmail.com>
2022-09-30 10:49:08 -0700

    EP check2

--- src/aot/experiments/Diagnostics/Logging/PortEventPipe/RunEP.bat
+++ src/aot/experiments/Diagnostics/Logging/PortEventPipe/RunEP.bat
@@ -1,7 +1,10 @@
 cls
 call taskkill /IM dotnet.exe /F
 git clean -xfd
-call build.cmd -arch x64 -os windows -s clr.aot -c Release > C:\Work\Core\LakshanF\CSharp\src\aot\experiments\Diagnostics\Logging\PortEventPipe\build\buildLog_change_41.log
+
+call build.cmd -arch x64 -os windows -s clr.aot+clr.nativeaotlibs -c Release > C:\Work\Core\LakshanF\CSharp\src\aot\experiments\Diagnostics\Logging\PortEventPipe\build\buildLog_change_100.log
+
+REM call build.cmd -arch x64 -os windows -s clr.aot -c Release > C:\Work\Core\LakshanF\CSharp\src\aot\experiments\Diagnostics\Logging\PortEventPipe\build\buildLog_change_49.log
 
 REM call build.cmd -arch x64 -os windows -s clr.aot -c Release -v:diag > C:\Work\Core\LakshanF\CSharp\src\aot\experiments\Diagnostics\Logging\PortEventPipe\build\buildLog_change_38.log
 
