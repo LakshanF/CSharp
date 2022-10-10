8b2b9fba5b09317295c82e103904e166496e9433
Lakshan Fernando <lakshanf@hotmail.com>
2022-10-10 13:50:26 -0700

    Build artifcats

--- src/aot/experiments/Diagnostics/Logging/PortEventPipe/RunEP.bat
+++ src/aot/experiments/Diagnostics/Logging/PortEventPipe/RunEP.bat
@@ -2,9 +2,13 @@ cls
 call taskkill /IM dotnet.exe /F
 git clean -xfd
 
-call build.cmd -arch x64 -os windows -s clr.aot+clr.nativeaotlibs -c Release > C:\Work\Core\LakshanF\CSharp\src\aot\experiments\Diagnostics\Logging\PortEventPipe\build\buildLog_change_100.log
+rem call build.cmd -arch x64 -os windows -s clr.aot+libs -c Release > C:\Work\Core\LakshanF\CSharp\src\aot\experiments\Diagnostics\Logging\PortEventPipe\build\buildLog_change_201.log
 
-REM call build.cmd -arch x64 -os windows -s clr.aot -c Release > C:\Work\Core\LakshanF\CSharp\src\aot\experiments\Diagnostics\Logging\PortEventPipe\build\buildLog_change_49.log
+REM call build.cmd -arch x64 -os windows -s clr.aot+libs+libs.tests -c Release /p:TestNativeAot=true /p:RunSmokeTestsOnly=true  > C:\Work\Core\LakshanF\CSharp\src\aot\experiments\Diagnostics\Logging\PortEventPipe\build\buildLog_change_503.log
+
+REM call build.cmd -arch x64 -os windows -s clr.aot+clr.nativeaotlibs -c Release > C:\Work\Core\LakshanF\CSharp\src\aot\experiments\Diagnostics\Logging\PortEventPipe\build\buildLog_change_100.log
+
+call build.cmd -arch x64 -os windows -s clr.aot -c Release > C:\Work\Core\LakshanF\CSharp\src\aot\experiments\Diagnostics\Logging\PortEventPipe\build\buildLog_change_576.log
 
 REM call build.cmd -arch x64 -os windows -s clr.aot -c Release -v:diag > C:\Work\Core\LakshanF\CSharp\src\aot\experiments\Diagnostics\Logging\PortEventPipe\build\buildLog_change_38.log
 
