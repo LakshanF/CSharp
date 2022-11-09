941f25a44574d01fb114e120d4a0566a8d8e9310
Lakshan Fernando <lakshanf@hotmail.com>
2022-11-09 05:03:28 -0800

    recent updates

--- src/aot/experiments/Diagnostics/Logging/EventSource/DotNet/Program.cs
+++ src/aot/experiments/Diagnostics/Logging/EventSource/DotNet/Program.cs
@@ -1,7 +1,7 @@
-﻿using System.Diagnostics.Tracing;
-
-namespace EventSourceDemo
+﻿namespace EP_Target
 {
+    using System.Diagnostics.Tracing;
+
     public static class Program
     {
         public static void Main(string[] args)
@@ -26,10 +26,13 @@ namespace EventSourceDemo
 
         [Event(1, Keywords = Keywords.Startup)]
         public void AppStarted(string message, int favoriteNumber) => WriteEvent(1, message, favoriteNumber);
+        
         [Event(2, Keywords = Keywords.Requests)]
         public void RequestStart(int requestId) => WriteEvent(2, requestId);
+        
         [Event(3, Keywords = Keywords.Requests)]
         public void RequestStop(int requestId) => WriteEvent(3, requestId);
+        
         [Event(4, Keywords = Keywords.Startup, Level = EventLevel.Verbose)]
         public void DebugMessage(string message) => WriteEvent(4, message);
 
@@ -40,4 +43,4 @@ namespace EventSourceDemo
             public const EventKeywords Requests = (EventKeywords)0x0002;
         }
     }
-}
+} 
