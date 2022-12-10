5f0e82c4381b51c65f276356b7eb604b056da25c
Lakshan Fernando <lakshanf@hotmail.com>
2022-12-10 13:46:08 -0800

    EventSource test

---
+++ src/Trim/EventSource/SourceGen/DiagnsoticsClient/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.Tracing;
+using System.Linq;
+
+using Microsoft.Diagnostics.NETCore.Client;
+using Microsoft.Diagnostics.Tracing.Parsers;
+using Microsoft.Diagnostics.Tracing;
+
+namespace EP_Client;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string processName = "reproNative";
+        if (args.Length > 0)
+        {
+            processName = args[0];
+        }
+        var intPid = Process.GetProcessesByName(processName).Single().Id;
+
+        Console.WriteLine($"Starting an StartEventPipeSession for Process:<{processName}>, PID:{intPid}");
+
+        PrintEventsLive(intPid);
+    }
+
+    public static void PrintEventsLive(int processId)
+    {
+        var providers = new List<EventPipeProvider>()
+        {
+            new EventPipeProvider("Test1",
+                EventLevel.Verbose, (long)ClrTraceEventParser.Keywords.Default)
+        };
+        var client = new DiagnosticsClient(processId);
+        using (var session = client.StartEventPipeSession(providers, false))
+        {
+
+            Task streamTask = Task.Run(() =>
+            {
+                var source = new EventPipeEventSource(session.EventStream);
+                source.Dynamic.All += (TraceEvent obj) =>
+                {
+                    Console.WriteLine($"Got event, Name: {obj.EventName}, Details: {obj}");
+                };
+                try
+                {
+                   source.Process();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error encountered while processing events");
+                    Console.WriteLine(e.ToString());
+                }
+            });
+
+            Task inputTask = Task.Run(() =>
+            {
+                Console.WriteLine("Press Enter to exit");
+                while (Console.ReadKey().Key != ConsoleKey.Enter)
+                {
+                    Thread.Sleep(100);
+                }
+                session.Stop();
+            });
+
+            Task.WaitAny(streamTask, inputTask);
+        }
+    }
+}
---
+++ src/Trim/EventSource/SourceGen/test1/Consumer/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ES_Test1;
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Waiting 10 seconds to client to get the PID");
+        Thread.Sleep(10 * 1000);
+        Console.WriteLine("Done Waiting");
+
+        TargetStartLogging();
+
+        Console.WriteLine("Done done!");
+    }
+
+    private static void TargetStartLogging()
+    {
+        Test1EventSource.Log.AppStarted("Hello World From .NET!", 12);
+        Test1EventSource.Log.DebugMessage("Got here From .NET!");
+        Test1EventSource.Log.DebugMessage("finishing startup From .NET!");
+        Test1EventSource.Log.AppInfo(new ClassTest(), new StructTest());
+        Test1EventSource.Log.RequestStart(3);
+        Test1EventSource.Log.RequestStop(3);
+    }
+}
