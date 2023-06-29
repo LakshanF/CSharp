c771251dec63187977d0a07ca6535c2374160c7a
Lakshan Fernando <lakshanf@hotmail.com>
2023-06-29 10:40:06 -0700

    FX events

--- src/Fun/HexGame/HexGame/HexGraph.cs
+++ src/Fun/HexGame/HexGame/HexGraph.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,9 +21,9 @@ namespace HexGame
 
   //  class hex_graph
   //  {
-  //      int size_one_side;
-  //      hex_node* nodes;
-  //      bool path_found(hex_node root);
+  //      int Length;
+  //      HexNode* Nodes;
+  //      bool path_found(HexNode root);
   //      vector<int> GetAdjacentNodes(int node_id);
   //      public:
 		//hex_graph(int size);
@@ -32,6 +35,12 @@ namespace HexGame
   //  };
 
 
+    internal enum HexState
+    {
+        Playing,
+        BlueWon,
+        RedWon
+    }
     internal class HexGraph
     {
         public int Length { get; init; }
@@ -39,6 +48,293 @@ namespace HexGame
         public HexGraph(int length)
         {
             Length = length;
+            Nodes = new List<HexNode>();
+            for (int i = 0; i < Length * Length; i++)
+            {
+                Nodes.Add(new HexNode() { Id=i });
+            }
+        }
+
+        public void PrintBoard()
+        {
+            for (int i = 0; i < Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                    Console.WriteLine("  "); // 2 spaces
+                if (i != 0)
+                {
+                    for (int j = 0; j < Length; j++)
+                    {
+                        Console.WriteLine("\\ /");
+                    }
+                    Console.WriteLine(Environment.NewLine);
+                }
+                for (int j = 0; j < i; j++)
+                    Console.WriteLine("  "); // 2 spaces
+                for (int j = 0; j < Length; j++)
+                {
+                    switch (Nodes[i * Length+ j].Piece)
+                    {
+                        case Piece.empty:
+                            Console.WriteLine(".");
+                            break;
+                        case Piece.blue:
+                            Console.WriteLine("B");
+                            break;
+                        case Piece.red:
+                            Console.WriteLine("R");
+                            break;
+                    }
+                    if (j != (Length - 1))
+                        Console.WriteLine(" - ");
+                }
+                Console.WriteLine(Environment.NewLine);
+            }
+        }
+
+        /// <summary>
+        /// Assumes first move, blue
+        /// </summary>
+        /// <param name="row"></param>
+        /// <param name="col"></param>
+        /// <returns></returns>
+        public bool MakeUserMove(int row, int col)
+        {
+            int position = row * Length + col;
+            if (Nodes[position].Piece != Piece.empty)
+                return false;
+            Nodes[position].Piece = Piece.blue;
+            return true;
+
+        }
+
+        /// <summary>
+        /// Dumb strategy first: make a move on an empty square
+        /// </summary>
+        /// <returns></returns>
+        public int MakeComputerMove()
+        {
+            for (int i = 0; i < Length * Length; i++)
+            {
+                if (Nodes[i].Piece == Piece.empty)
+                {
+                    Nodes[i].Piece = Piece.red;
+                    return Nodes[i].Id;
+                }
+            }
+            return -1;
+        }
+
+        /// <summary>
+        /// Blue goes first, and need to create a path from east-west
+        /// Red goes from south-north
+        /// 0 - playing
+        /// 1 - blue won
+        /// 2 - red won
+        /// </summary>
+        /// <returns></returns>
+        public HexState GameState()
+        {
+            // short cut first to see that all columns (blue) or rows (red) have at least 
+
+            // blue first
+            bool blue_found = false;
+            for (int i = 0; i < Length; i++)
+            {
+                blue_found = false;
+                for (int j = 0; j < Length; j++)
+                {
+                    int col_pos = (j * Length) + i;
+                    if (Nodes[col_pos].Piece == Piece.blue)
+                    {
+                        blue_found = true;
+                        break;
+                    }
+                }
+                if (!blue_found)
+                    break;
+            }
+
+            if (blue_found)
+            {
+                // need to check with a breadth first search if there is a path
+                // We will look at all the boxes in the left most column and check if there is a path to the right most column
+                for (int i = 0; i < Length; i++)
+                {
+                    int col_pos = i * Length;
+                    if (Nodes[col_pos].Piece == Piece.blue)
+                    {
+                        if (EndToEndPathFound(Nodes[col_pos]))
+                            return HexState.BlueWon;
+                    }
+                }
+            }
+
+            // red next
+            bool red_found = false;
+            for (int i = 0; i < Length; i++)
+            {
+                red_found = false;
+                for (int j = 0; j < Length; j++)
+                {
+                    int red_pos = i * Length + j;
+                    if (Nodes[red_pos].Piece == Piece.red)
+                    {
+                        red_found = true;
+                        break;
+                    }
+                }
+                if (!red_found)
+                    break;
+            }
+
+            if (red_found)
+            {
+                // need to check with a breadth first search if there is a path
+                // We will look at all the boxes in the first row and check if any has reached the last rown
+                for (int i = 0; i < Length; i++)
+                {
+                    int row_pos = i;
+                    if (Nodes[row_pos].Piece == Piece.red)
+                    {
+                        if (EndToEndPathFound(Nodes[row_pos]))
+                            return HexState.RedWon;
+                    }
+                }
+            }
+            return 0;
+        }
+
+        /// <summary>
+        /// Given a starting position (westmost column or southmost row), determine if there is a traversing path
+        /// Blue travels from east to west (right)
+        /// Red travels from north to south (down)
+        /// Algorithm (Breadth search first, with dynamically getting adjacency nodes based on the position)
+        ///  - Make all the nodes white
+        ///  - Add source to Q
+        ///  - While Q is not empty
+        ///		+ Deque
+        ///		+ if node is on the opposite side, return as true
+        ///		+ Color node black
+        ///		+ for all white adjacency nodes with the same piece
+        ///			+ color node grey
+        ///			+ add to Q
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        internal bool EndToEndPathFound(HexNode root)
+        {
+            Queue<HexNode> node_queue = new();
+            for (int i = 0; i < Length * Length; i++)
+            {
+                Nodes[i].Color = PathColor.white;
+            }
+            root.Color = PathColor.grey;
+            node_queue.Enqueue(root);
+
+            while (node_queue.Count != 0)
+            {
+                HexNode node = node_queue.Dequeue();
+                if (node.Piece == Piece.blue && (node.Id % Length == (Length - 1)))
+                    return true;
+                if (node.Piece == Piece.red && (node.Id >= (Length * Length - Length)))
+                    return true;
+                List<int> adjacentIds = GetAdjacentNodes(node.Id);
+
+                // @TODO: foreach?
+                foreach(int i in adjacentIds)
+                {
+                    if (Nodes[i].Piece == node.Piece && Nodes[i].Color == PathColor.white)
+                    {
+                        Nodes[i].Color = PathColor.grey;
+                        node_queue.Enqueue(Nodes[i]);
+                    }
+                }
+                node.Color = PathColor.black;
+            }
+            return false;
+        }
+
+        internal List<int> GetAdjacentNodes(int nodeId)
+        {
+            List<int> ids = new();
+
+            // based on the nodeId, we return the neighbor nodes
+
+            // Corners
+            if (nodeId == 0)
+            {
+                // corner (2 edges): top left
+                ids.Add(1);
+                ids.Add(Length);
+            }
+            else if (nodeId == (Length * Length - 1))
+            {
+                // corner (2 edges): bottom right
+                ids.Add(Length * Length - 2);
+                ids.Add(Length * Length - 1 - Length);
+            }
+            else if (nodeId == (Length - 1))
+            {
+                // corner (3 edges): top right
+                ids.Add(Length - 2);
+                ids.Add(Length - 1 + Length);
+                ids.Add(Length - 1 + Length - 1);
+            }
+            else if (nodeId == (Length * Length - Length))
+            {
+                // corner (3 edges): bottom left
+                ids.Add(Length * Length - Length + 1);
+                ids.Add(Length * Length - Length - Length);
+                ids.Add(Length * Length - Length - Length + 1);
+            }
+            // Edges
+            else if (nodeId % Length == 0)
+            {
+                // left (4 edges)
+                ids.Add(nodeId + Length);
+                ids.Add(nodeId - Length);
+                ids.Add(nodeId - Length + 1);
+                ids.Add(nodeId + 1);
+            }
+            else if (nodeId % Length == (Length - 1))
+            {
+                // Right (4 edges)
+                ids.Add(nodeId + Length);
+                ids.Add(nodeId + Length - 1);
+                ids.Add(nodeId - Length);
+                ids.Add(nodeId - 1);
+            }
+            else if (nodeId < (Length - 1))
+            {
+                // top (4 edges)
+                ids.Add(nodeId + 1);
+                ids.Add(nodeId - 1);
+                ids.Add(nodeId + Length);
+                ids.Add(nodeId + Length - 1);
+            }
+            else if (nodeId > (Length * Length - Length))
+            {
+                // bottom (4 edges)
+                ids.Add(nodeId + 1);
+                ids.Add(nodeId - 1);
+                ids.Add(nodeId - Length);
+                ids.Add(nodeId - Length + 1);
+            }
+            // Middle nodes
+            else
+            {
+                // (6 edges)
+                ids.Add(nodeId + 1);
+                ids.Add(nodeId - 1);
+                ids.Add(nodeId - Length);
+                ids.Add(nodeId - Length + 1);
+                ids.Add(nodeId + Length);
+                ids.Add(nodeId + Length - 1);
+            }
+
+            return ids;
+
         }
     }
 }
---
+++ src/aot/experiments/Diagnostics/Logging/EventSource/RuntimeLibraries/NetHttp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using System.Diagnostics.Tracing;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+
+    // sealed class EventSourceCreatedListener : EventListener
+    // {
+    //     protected override void OnEventSourceCreated(EventSource eventSource)
+    //     {
+    //         base.OnEventSourceCreated(eventSource);
+    //         Console.WriteLine($"New event source: {eventSource.Name}");
+    //     }
+    // }    
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // EventSourceCreatedListener el = new EventSourceCreatedListener();
+            Console.WriteLine("Waiting 10 seconds to client to get the PID");
+            Thread.Sleep(10*1000);
+            await M1();
+            Console.WriteLine("Waiting for tool to do its thing");
+            Thread.Sleep(10*1000);
+            Console.WriteLine("Completed");
+        }
+
+        static async Task M1()
+        {
+            using var httpClient = new HttpClient();
+            string response = await httpClient.GetStringAsync("http://redhatloves.net/");
+            Console.WriteLine($"Received response with length {response.Length}");
+        }
+    }
+}
