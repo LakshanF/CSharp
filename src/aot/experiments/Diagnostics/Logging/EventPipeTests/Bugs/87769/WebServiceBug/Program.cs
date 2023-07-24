625cd14bdb6c8dc91f004b3cd96b9d4de3173f3e
Lakshan Fernando <lakshanf@hotmail.com>
2023-07-24 12:23:06 -0700

    ETW Issue

---
+++ src/aot/experiments/Diagnostics/Logging/EventPipeTests/Bugs/87769/WebServiceBug/Program.cs
@@ -0,0 +1,31 @@
+using System.Text.Json.Serialization;
+using WebServiceBug;
+
+var builder = WebApplication.CreateSlimBuilder(args);
+
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
+});
+
+var app = builder.Build();
+
+//builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+var sampleTodos = TodoGenerator.GenerateTodos().ToArray();
+
+var todosApi = app.MapGroup("/todos");
+todosApi.MapGet("/", () => sampleTodos);
+todosApi.MapGet("/{id}", (int id) =>
+    sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
+        ? Results.Ok(todo)
+        : Results.NotFound());
+
+app.Run();
+
+[JsonSerializable(typeof(Todo[]))]
+internal partial class AppJsonSerializerContext : JsonSerializerContext
+{
+
+}
