e64c623b44446bc36a1e2d1fe43e32e0e9e6c77f
Lakshan Fernando <lakshanf@hotmail.com>
2023-08-08 15:36:33 -0700

    additional provider tests

---
+++ src/aot/experiments/Diagnostics/Logging/EventPipeTests/WellKnownProviders/MicrosoftExtensionsDependencyInjectionTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Wait for 10 seconds");
+        Thread.Sleep(10*1000);
+
+        var serviceProvider = new ServiceCollection()
+            .AddSingleton<IMyService, MyService>()
+            .BuildServiceProvider();
+
+        var myService = serviceProvider.GetService<IMyService>();
+        myService.DoSomething();
+    }
+}
+
+public interface IMyService
+{
+    void DoSomething();
+}
+
+public class MyService : IMyService
+{
+    public void DoSomething()
+    {
+        Console.WriteLine("Hello from MyService!");
+    }
+}
---
+++ src/aot/experiments/Diagnostics/Logging/EventPipeTests/WellKnownProviders/SystemNetHttpTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Threading;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Wait for 10 seconds");
+        Thread.Sleep(10*1000);
+
+        using var client = new HttpClient();
+
+        var response = await client.GetAsync("https://jsonplaceholder.typicode.com/todos/1");
+
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(content);
+        }
+    }
+}
