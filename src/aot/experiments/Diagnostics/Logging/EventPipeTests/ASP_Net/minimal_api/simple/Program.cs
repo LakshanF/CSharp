137bfb53d7cbae09e361df994b8939a92616c44c
Lakshan Fernando <lakshanf@hotmail.com>
2023-08-09 05:52:05 -0700

    asp_net samples

---
+++ src/aot/experiments/Diagnostics/Logging/EventPipeTests/ASP_Net/minimal_api/simple/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+// 
+
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.MapGet("hello", () => "Hello, World");
+
+app.Run();
