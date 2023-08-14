6aeba86e1e3789f4eec763c073af3d0da473864c
Lakshan Fernando <lakshanf@hotmail.com>
2023-08-14 08:47:59 -0700

    minor updates

--- src/aot/experiments/Diagnostics/Logging/EventPipeTests/Metrics/metric-instr/Program.cs
+++ src/aot/experiments/Diagnostics/Logging/EventPipeTests/Metrics/metric-instr/Program.cs
@@ -1,19 +1,33 @@
-﻿using System.Diagnostics.Metrics;
+﻿using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using System.Diagnostics.Metrics;
 
 class Program
 {
     static Meter s_meter = new("HatCo.HatStore", "1.0.0");
-    static Counter<int> s_hatsSold = s_meter.CreateCounter<int>("hats-sold");
+    static Counter<int> s_hatsSold = s_meter.CreateCounter<int>(
+        name: "hats-sold",
+        unit: "Hats",
+        description: "The number of hats sold in our store");
 
     static void Main(string[] args)
     {
+        using MeterProvider meterProvider = Sdk.CreateMeterProviderBuilder()
+                .AddMeter("HatCo.HatStore")
+                .AddPrometheusExporter(opt =>
+                {
+                    opt.StartHttpListener = true;
+                    opt.HttpListenerPrefixes = new string[] { $"http://localhost:9184/" };
+                })
+                .Build();
+
         var rand = Random.Shared;
         Console.WriteLine("Press any key to exit");
         while (!Console.KeyAvailable)
         {
             //// Simulate hat selling transactions.
             Thread.Sleep(rand.Next(100, 2500));
-            s_hatsSold.Add(rand.Next(0, 1000));
+            s_hatsSold.Add(rand.Next(0,1000));
         }
     }
 }
