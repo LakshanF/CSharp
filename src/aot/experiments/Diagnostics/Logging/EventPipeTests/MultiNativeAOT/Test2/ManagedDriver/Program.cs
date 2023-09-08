a338a4bdad803939b27e27eaf8d3efbe78c69520
Lakshan Fernando <lakshanf@hotmail.com>
2023-09-08 11:30:13 -0700

    checkpoint

---
+++ src/aot/experiments/Diagnostics/Logging/EventPipeTests/MultiNativeAOT/Test2/ManagedDriver/Program.cs
@@ -0,0 +1,58 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Diagnostics.Tracing;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace ManagedDriverNS
+{
+    public sealed class LaksManagedEventSource : EventSource
+    {
+        private LaksManagedEventSource() {}
+        public static LaksManagedEventSource Log = new LaksManagedEventSource();
+        public void MyEvent() { WriteEvent(1, "MyEvent"); }
+    }
+
+    public class ManagedDriver
+    {
+        public static int Main()
+        {
+            // This test (temp?) validates event behavior via dotnet-trace and perfview
+
+            Console.WriteLine("Waiting 10 seconds to client to get the PID");
+            Thread.Sleep(10*1000);
+
+            // GC.Collect();
+            // List<Foo> list = new List<Foo>();
+
+            // for (int i = 0; i < 100000; i++)
+            // {
+            //     list.Add(new Foo(i));
+            //     if (i % 10000 == 0)
+            //         Console.WriteLine($"Fired MyEvent {i:N0}/100,000 times...");
+            //     LaksManagedEventSource.Log.MyEvent();
+            // }
+            // int rndValue = new Random().Next(0, 100000);
+            // Console.WriteLine($"{list[rndValue].IValue}-{list[rndValue].SValue}");
+            // GC.Collect();
+
+            Console.WriteLine("Waiting 10 seconds to EventPipe to write the data");
+            Thread.Sleep(10*1000);
+
+            return 100;
+        }
+    }
+
+    // public class Foo
+    // {
+    //     public int IValue{get;set;}
+    //     public string SValue{get;set;}
+    //     public Foo(int value)
+    //     {
+    //         IValue = value;
+    //         SValue = value.ToString();
+    //     }
+    // }
+}
---
+++ src/aot/experiments/Diagnostics/Logging/EventPipeTests/MultiNativeAOT/Test2/NativeLibrary1/NativeLibrary1.cs
@@ -0,0 +1,18 @@
+﻿namespace NativeLibrary1;
+
+public class MultiRtSum
+{
+    [UnmanagedCallersOnly(EntryPoint = "addInLib1")]
+    public static int AddInLib1(int a, int b)
+    {
+        Console.WriteLine("Waiting 10 seconds to client to get the PID");
+        Thread.Sleep(10*1000);
+
+        for (int i = 0; i < 100; i++)
+        {
+            LaksDemoEventSource.Log.MyEvent();
+        }
+
+        return a + b;
+    }
+}
