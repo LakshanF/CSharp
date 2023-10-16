5aaffa5b7c9a5613e6d731ac0b2001a17be25925
Lakshan Fernando <lakshanf@hotmail.com>
2023-10-16 16:30:33 -0700

    parallel take 1

---
+++ src/aot/experiments/Diagnostics/Logging/EcoSystem/driver/NugetPkg2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoSystemDriver
+{
+    internal class NugetPkg2
+    {
+        public string? PkgHash { get; set; }
+        public string? Id { get; set; }
+        public string? ContainerPath { get; set; }
+        public string? Version { get; set; }
+
+        public NugetPkg2(string line)
+        {
+            var values = line.Split(',');
+            PkgHash = values[0];
+            Id = values[1];
+            ContainerPath = values[2];
+            Version = values[3];
+
+        }
+    }
+}
